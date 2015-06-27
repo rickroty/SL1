1df82ac518f30b21a6a9706fc355395f43249d68
rickroty <github.ok@roty.com>
2015-06-27 11:35:33 -0700

    Update voltmeter.py

--- voltmeter.py
+++ voltmeter.py
@@ -4,6 +4,9 @@ import RPi.GPIO as GPIO
 import time
 from time import sleep
 import datetime
+import socket
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 SWITCH = 18
 VOLTMETER = 15
@@ -23,7 +26,11 @@ while (run):
         if meteron:
              meteron = False
              GPIO.output(VOLTMETER, GPIO.LOW)
+            clientsocket.connect(('localhost', 5000))
+            clientsocket.send('killusb')
+            clientsocket.close()             
              print 'VOLT METER OFF'
+             print 'USB OFF'
              sleep(0.5)
         else:
              meteron = True
